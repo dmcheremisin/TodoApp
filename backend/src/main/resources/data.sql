f35c00dc040533c69afad0e011f4a7717c9c953c
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-04-16 17:56:59 +0300

    todo done checkbox

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,7 +4,7 @@ insert into todos (id, user, description, date, done) VALUES
     (null, 'dima', 'Learn Microservices', sysdate(), false);
 
 INSERT INTO users VALUES
-    (null, 'dima', '$2a$10$B.0xV52aBkWgWTABWqQ4D.t8FbLp2yrl/2V6QQxWsxzlziDAauVmC', true);
+    (null, 'dima', '$2a$10$YNkidE/CGose4bmydBsAQuxJ5dKBFLc/IbHfQOf9Rybubr5YiAcq6', true);
 
 INSERT INTO roles VALUES
     (null, 'dima', 'ROLE_ADMIN');
